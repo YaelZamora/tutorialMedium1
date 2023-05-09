f8f2604a6e60fc30b5c1908652afc25303ef7768
YaelZamora <ajedrezkasparov@gmail.com>
2023-05-09 10:41:53 -0500

    Initial Commit

---
+++ tutorial/tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorialApp.swift
+//  tutorial
+//
+//  Created by MacMini on 09/05/23.
+//
+
+import SwiftUI
+
+@main
+struct tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
